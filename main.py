4f95d0354fb88db02aaea121df4351ed59513fc9
HackerGoose <peilinrao.work@gmail.com>
2025-08-29 15:44:35 -0700

    main.py skeleton

--- main.py
+++ main.py
@@ -12,33 +12,40 @@ Notes:
 import hydra
 from omegaconf import DictConfig
 
+from utils import TimeKeeper
+from src import pipeline_init
+from src import configurator_init
+from src import wandb_init
+from src import train_loop_init
+
+# NOTE: set verbose=0 to print no config; 
+# verbose=1 to print the final config; 
+# verbose=2 to print both initial and final config.
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def main(config: DictConfig) -> None:
-    # NOTE: set verbose=0 to print no config; 
-    # verbose=1 to print the final config; 
-    # verbose=2 to print both initial and final config.
-    config = init_config(config, verbose=2)
     
-    # NOTE: customize each component in `_src` if needed.
-    train_state, optimizer, train_loader, loss_fn, logger, wandb_logger = init_pipeline(config)
-
-    time_keeper = TimeKeeper()
-
-    init_wandb(config)
+    # step 1: initialize configuration
+    # fool-proof preprocessing and sanity checks
+    config = configurator_init(config, verbose=2)
     
-    # NOTE: customize your own train_loop in `_src/train`.
-    # train_loop = init_train_loop(config)
-    lm_train_loop(
+    # step 2: initialize pipeline
+    # all components in the pipeline can be customized in their corresponding directories
+    train_state, optimizer, train_loader, loss_fn, logger, wandb_logger = pipeline_init(config)
+
+    # step 3: initialize wandb (weights and bias)
+    wandb_init(config)
+    
+    # step 4: initialize train loop, from the components in the pipeline
+    train_loop_init(
         config = config,
         train_state = train_state,
         optimizer = optimizer,
         dataloader = train_loader,
         loss_fn = loss_fn,
         logger = logger,
-        time_keeper = time_keeper,
+        time_keeper = TimeKeeper(),
         wandb_logger = wandb_logger,
     )
-
 
 if __name__ == "__main__":
     main()
