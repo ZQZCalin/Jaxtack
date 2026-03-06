b18e7dbcdd639a09f127007cc919f3fd1d208962
Qinzi Zhang <zqz20100221@126.com>
2026-03-05 19:10:38 -0500

    update local setup

--- setup.sh
+++ setup.sh
@@ -1,16 +1,31 @@
 #!/bin/bash -l
 
-if [ "$1" = "local" ]; then
-    REQ_FILE="deploy/requirements_local.txt"
-elif [ "$1" = "" ] || [ "$1" = "scc" ]; then
+setup_local() {
     module load python3/3.10.12 cuda/12.2
-    [ ! -d "env" ] && python -m venv env
-    source env/bin/activate
+    [ ! -d ".venv" ] && python -m venv .venv
+    . .venv/bin/activate
+    REQ_FILE="deploy/requirements_local.txt"
+    pip install -r "$REQ_FILE"
+}
+
+setup_scc() {
+    module load python3/3.10.12 cuda/12.2
+    [ ! -d ".venv" ] && python -m venv .venv
+    source .venv/bin/activate
     pip install --upgrade "jax[cuda12]==0.4.31"
     REQ_FILE="deploy/requirements_scc.txt"
-else
+    pip install -r "$REQ_FILE"
+}
+
+setup_error() {
     echo "Usage: $0 [local|scc]"
-    exit 1
+}
+
+if [ "$1" = "local" ]; then
+    setup_local
+elif [ "$1" = "scc" ]; then
+    setup_scc
+else
+    setup_error
 fi
 
-pip install -r $REQ_FILE
